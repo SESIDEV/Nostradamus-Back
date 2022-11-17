d0985c61f62cb5aa1cd3c7727056c067061d010f
Bloutware <iurykoz@gmail.com>
2022-11-17 16:21:09 -0300

    loop SD e fix thread duplo setando debug false

--- files/conector.py
+++ files/conector.py
@@ -2,7 +2,7 @@ import mysql.connector as mydb
 
 #STRINGS SQL:
 set_in_process = "UPDATE requests SET inProcess = 1 WHERE id = %s"
-set_done = "UPDATE requests SET inProcess = 0 AND done = 1 WHERE input = %s"
+set_done = "UPDATE requests SET inProcess = 0, done = 1 WHERE id = %s"
 add_request = "INSERT INTO requests (id, input, inProcess, done, fastSearch, selectedBases) VALUES (%s, %s, %s, %s, %s, %s)"
 add_research = "INSERT INTO pesquisas (id, json) VALUES (%s, %s)"
 lookup_reserach = "SELECT json FROM pesquisas WHERE id = %s"
@@ -11,10 +11,10 @@ get_pending = "SELECT id FROM requests WHERE done = 0 AND inProcess = 0"
 get_full_request = "SELECT input, fastSearch, selectedBases FROM requests WHERE id = %s"
 
 def retornarDB():
-    print("Conectando ao banco...")
+    print("\nConectando ao banco...")
     db = mydb.connect(host="containers-us-west-110.railway.app", port="7717", user="root",password="LyTaKtOjN58WLwYREa5Z",database="railway")
     #db = mydb.connect(host="localhost",user="root",password="admin",database="railway")
-    print("Conectado.")
+    print("Conectado.\n")
     return db
 
 def prepararParaPesquisa(token): #   ESTÁGIO 2 (RETORNA DADOS NECESSÁRIOS PARA EFETUAR A BUSCA COMPLETA)
@@ -35,9 +35,11 @@ def incluirNoBanco(token, json): #   ESTÁGIO 3
     cursor = db.cursor()
     parametros = (token, json)
     cursor.execute(add_research, parametros) # ADICIONA A BUSCA COMPLETA NA TABELA DE PESQUISAS
+    print('\nPesquisa adicionada com sucesso.')
     parametros2 = (token,)
     cursor.execute(set_done, parametros2) # ALTERA O STATUS DO REQUEST PARA CONCLUÍDO
     db.commit()
+    print('Status alterado para concluído.\n')
 
 def salvarRequisicao(token, termo_busca, busca_rapida, bases): #   ESTÁGIO 0
     db = retornarDB()
